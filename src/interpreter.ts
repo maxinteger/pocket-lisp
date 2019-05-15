54f7a21fd987e24870a8d7f6fb20d9540de3396d
Vadasz Laszlo <maxinteger@gmail.com>
2019-05-15 22:26:53 +0100

    move all literals into the stdlib + define minimal implementation

--- src/interpreter.ts
+++ src/interpreter.ts
@@ -4,21 +4,71 @@ import { RuntimeError } from 'dataTypes/RuntimeError'
 import { nativeFn } from 'stdlib/utils'
 import { PLCallable } from 'types'
 
+interface PLLiteral {
+  parser: (x: string) => any
+  factory: PLCallable
+}
+
 export interface InterpreterOptions {
   globals?: { [key: string]: any }
   stdout?: (out: string) => void
   lockedGlobals?: boolean
 }
 
-const defaultOptions = { stdout: undefined, lockedGlobals: true, globals: <any>{} }
+export interface PLLiterals {
+  bool: PLLiteral
+  int: PLLiteral
+  float: PLLiteral
+  fractionNumber: PLLiteral
+  vector: PLLiteral
+  hashMap: PLLiteral
+}
+
+const notImplementedLiteral = (name: string): PLLiteral => ({
+  parser: () => {
+    throw new RuntimeError(`${name} is not implemented`)
+  },
+  factory: nativeFn(() => {
+    throw new RuntimeError(`${name} is not implemented`)
+  })
+})
+
+const defaultLiterals: PLLiterals = {
+  bool: {
+    parser: (x: string) => x === 'true',
+    factory: nativeFn(Boolean)
+  },
+  int: {
+    parser: (x: string) => parseInt(x, 10),
+    factory: nativeFn(Number)
+  },
+  float: {
+    parser: parseFloat,
+    factory: nativeFn(Number)
+  },
+  fractionNumber: notImplementedLiteral('FractionNumber'),
+  vector: notImplementedLiteral('Vector'),
+  hashMap: notImplementedLiteral('HashMap')
+}
+
+const defaultOptions = {
+  stdout: undefined,
+  lockedGlobals: true,
+  globals: <any>{}
+}
 
 ///
 
 export class Interpreter {
   private readonly globals = new Environment()
   private _currentEnv = this.globals
-  constructor(options?: InterpreterOptions) {
+  constructor(options?: InterpreterOptions, literals?: PLLiterals) {
     const { stdout, globals, lockedGlobals } = { ...defaultOptions, ...options }
+    const plLiterals = { ...literals, ...defaultLiterals }
+
+    Object.keys(plLiterals).forEach(key => {
+      this.globals.define(key, (plLiterals as any)[key].factory)
+    })
 
     this.globals.define('print', nativeFn(stdout || console.log))
 
