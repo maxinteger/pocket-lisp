68bfd52318f6479e3beb35977620a0220f003e03
Vadasz Laszlo <maxinteger@gmail.com>
2019-06-04 19:43:01 +0100

    fix RuntimeError stack trace

--- src/lang/dataTypes/RuntimeError.ts
+++ src/lang/dataTypes/RuntimeError.ts
@@ -1,3 +1,10 @@
 export class RuntimeError extends Error {
-
+  constructor(error: Error | string) {
+    if (typeof error === 'string') {
+  		super(error)
+    } else {
+    	super(error.message)
+    	this.stack = error.stack
+		}
+  }
 }
